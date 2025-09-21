54c4997e49957ffa3681bc5c904748580c3c9b21
Abdul-Aziz <abdul-aziz@Abdul-Rahim.local>
2025-09-21 10:55:34 +0300

    feat: add basic vite config

---
+++ portfolio/vite.config.js
@@ -0,0 +1,44 @@
+import { defineConfig } from 'vite';
+import autoprefixer from 'autoprefixer';
+import sortMediaQueries from 'postcss-sort-media-queries';
+import { imagetools } from 'vite-imagetools';
+import { resolve } from 'path';
+
+export default defineConfig(({ mode }) => {
+  return {
+    root: resolve(__dirname, 'src'),
+    publicDir: resolve(__dirname, 'public'),
+    // base: mode === 'production' ? '/azizshik-JSFE2025Q3/' : '/',
+    base: '/',
+    plugins: [imagetools()],
+    css: {
+      postcss: {
+        plugins: [autoprefixer(), sortMediaQueries()],
+      },
+      preprocessorOptions: {
+        scss: {},
+      },
+    },
+    build: {
+      outDir: resolve(__dirname, 'dist'),
+      minify: false,
+      cssMinify: false,
+      rollupOptions: {
+        input: resolve(__dirname, 'src/index.html'),
+        output: {
+          entryFileNames: 'js/[name].js',
+          chunkFileNames: 'js/[name].js',
+          assetFileNames: ({ name }) => {
+            if (/\.(css|scss)$/.test(name ?? '')) {
+              return 'css/[name][extname]';
+            }
+            if (/\.(jpe?g|png|webp|avif|gif)$/.test(name ?? '')) {
+              return 'assets/images/[name][extname]';
+            }
+            return 'assets/[ext]/[name][extname]';
+          },
+        },
+      },
+    },
+  };
+});
